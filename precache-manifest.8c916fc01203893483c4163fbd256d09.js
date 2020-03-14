7b3029ef822690f7b7a40ba46d46fcc8791a5fec
minhhp2410 <60665582+minhhp2410@users.noreply.github.com>
2020-03-14 17:26:35 +0700

    Updates

---
+++ precache-manifest.8c916fc01203893483c4163fbd256d09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd3d88479835697add98f542ac31f16",
+    "url": "/react1/index.html"
+  },
+  {
+    "revision": "0be0e058eff1f1ae3163",
+    "url": "/react1/static/css/main.b7896666.chunk.css"
+  },
+  {
+    "revision": "222a9e098f0bd50aa10a",
+    "url": "/react1/static/js/2.97676ec3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react1/static/js/2.97676ec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0be0e058eff1f1ae3163",
+    "url": "/react1/static/js/main.7ed012e2.chunk.js"
+  },
+  {
+    "revision": "0eb8e3222e0bdd452196",
+    "url": "/react1/static/js/runtime-main.11c3e735.js"
+  }
+]);
